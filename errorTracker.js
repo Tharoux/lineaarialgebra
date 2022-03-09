d57a19ff91675146d0e4a741be309f9a52b2265f
Tharoux <65469358+Tharoux@users.noreply.github.com>
2022-03-10 01:28:59 +0200

    Add files via upload

--- errorTracker.js
+++ errorTracker.js
@@ -1,110 +1,110 @@
-function jaxMathError(id, type) {
-	window.addEventListener('load', e => {
+// function jaxMathError(id, type) {
+	// window.addEventListener('load', e => {
 		
-		let error 
-				= (document.getElementsByClassName(id).length > 0) 
-				? true 
-				: false;
+		// let error 
+				// = (document.getElementsByClassName(id).length > 0) 
+				// ? true 
+				// : false;
 		
-		if (type == 'tag') {
-			error = (document.getElementsByTagName(id).length > 0)
-			? true
-			: false;
-		}
+		// if (type == 'tag') {
+			// error = (document.getElementsByTagName(id).length > 0)
+			// ? true
+			// : false;
+		// }
 		
-		let errorMessage = document.createElement('span');
-		errorMessage.classList.add('error-msg');
-		errorMessage.innerHTML = 'Tapahtui virhe.';
+		// let errorMessage = document.createElement('span');
+		// errorMessage.classList.add('error-msg');
+		// errorMessage.innerHTML = 'Tapahtui virhe.';
 		
-		let errorClose = document.createElement('span');
-		errorClose.classList.add('error-button');
-		errorClose.innerHTML = 'Sulje';
+		// let errorClose = document.createElement('span');
+		// errorClose.classList.add('error-button');
+		// errorClose.innerHTML = 'Sulje';
 		
-		let errorUpdate = document.createElement('span');
-		errorUpdate.classList.add('error-button');
-		errorUpdate.addEventListener('click', e => {
-			window.location.reload();
-		});
-		errorUpdate.innerHTML = 'Päivitä sivu';
+		// let errorUpdate = document.createElement('span');
+		// errorUpdate.classList.add('error-button');
+		// errorUpdate.addEventListener('click', e => {
+			// window.location.reload();
+		// });
+		// errorUpdate.innerHTML = 'Päivitä sivu';
 		
-		let errorContainer = document.createElement('div');
-		errorContainer.classList.add('error-container');
-		errorContainer.append(errorMessage, errorUpdate, errorClose);
+		// let errorContainer = document.createElement('div');
+		// errorContainer.classList.add('error-container');
+		// errorContainer.append(errorMessage, errorUpdate, errorClose);
 		
-		let errorBox = document.createElement('div');
-		errorBox.classList.add('jax-error');
-		errorClose.addEventListener('click', e => {
-			for (var o of document.getElementsByClassName('jax-error')) {
-				o.remove();
-			}
-		});
-		errorBox.append(errorContainer);
+		// let errorBox = document.createElement('div');
+		// errorBox.classList.add('jax-error');
+		// errorClose.addEventListener('click', e => {
+			// for (var o of document.getElementsByClassName('jax-error')) {
+				// o.remove();
+			// }
+		// });
+		// errorBox.append(errorContainer);
 		
-		let searchString;
-		switch (type) {
-			case 'attribute':
-				searchString = `[class*='${id}']`;
-			break;
-			case 'tag':
-				searchString = `${id}`;
-			break;
-		}
+		// let searchString;
+		// switch (type) {
+			// case 'attribute':
+				// searchString = `[class*='${id}']`;
+			// break;
+			// case 'tag':
+				// searchString = `${id}`;
+			// break;
+		// }
 		
-		const config = {attributes: true, childList: true, subtree: true}
+		// const config = {attributes: true, childList: true, subtree: true}
 
-		const callback = function(mutationRecord, observer) {
-			// console.log(mutationRecord);
+		// const callback = function(mutationRecord, observer) {
+			// // console.log(mutationRecord);
 			
-			for (const record of mutationRecord) {
-				if (record.type == 'childList') {
-					for (const child of record.target.childNodes) {
-						if (child.className == 'MathJax_Error' && 
-							!document.getElementsByClassName('jax-error')) {
-							document.body.appendChild(errorBox);
-						}
+			// for (const record of mutationRecord) {
+				// if (record.type == 'childList') {
+					// for (const child of record.target.childNodes) {
+						// if (child.className == 'MathJax_Error' && 
+							// !document.getElementsByClassName('jax-error')) {
+							// document.body.appendChild(errorBox);
+						// }
 						
-						if (child.tagName == 'MJX-CONTAINER' && 
-							!document.getElementsByClassName('jax-error')) {
-							document.body.appendChild(errorBox);
-						}
-					}
-				}
-			}
-		}
-		
-		const observers = [];
-		
-		setTimeout(
-			function() {
-				// if (error) {
-					// console.info('MathJax errors are now observed');
+						// if (child.tagName == 'MJX-CONTAINER' && 
+							// !document.getElementsByClassName('jax-error')) {
+							// document.body.appendChild(errorBox);
+						// }
+					// }
 				// }
-				// document.querySelectorAll(searchString).forEach(o => {
-					
-					// var observer = new MutationObserver(callback);
-					// observers.push(observer);
-					
-					// observer.observe(o, config);
-				// })
-				console.info('Checking for MathJax errors...');
-				if (document.querySelectorAll('[class*="MathJax_Error"]').length > 0) {
-					document.body.appendChild(errorBox);
-				}
-			}
-		, 25000);
+			// }
+		// }
+		
+		// const observers = [];
 		
 		// setTimeout(
 			// function() {
-				// if (error) {
-					// console.info('MathJax errors are no longer observed');
+				// // if (error) {
+					// // console.info('MathJax errors are now observed');
+				// // }
+				// // document.querySelectorAll(searchString).forEach(o => {
+					
+					// // var observer = new MutationObserver(callback);
+					// // observers.push(observer);
+					
+					// // observer.observe(o, config);
+				// // })
+				// console.info('Checking for MathJax errors...');
+				// if (document.querySelectorAll('[class*="MathJax_Error"]').length > 0) {
+					// document.body.appendChild(errorBox);
 				// }
-				// observers.forEach(o => {
-					// o.disconnect();
-				// });
 			// }
-		// , 120000);
-	});
-}
+		// , 25000);
+		
+		// // setTimeout(
+			// // function() {
+				// // if (error) {
+					// // console.info('MathJax errors are no longer observed');
+				// // }
+				// // observers.forEach(o => {
+					// // o.disconnect();
+				// // });
+			// // }
+		// // , 120000);
+	// });
+// }
 
-jaxMathError('nolink', 'attribute');
-jaxMathError('mjx-container', 'tag');
+// jaxMathError('nolink', 'attribute');
+// jaxMathError('mjx-container', 'tag');
